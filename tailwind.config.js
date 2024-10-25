1ded36bfc06e483eba529afc9a480c04a0ff434f
jaheemprevost <jaheemdp@gmail.com>
2024-10-24 21:11:50 -0400

    Setup initial project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary_red: '#FC4747',
+        primary_white: '#FFFFFF',
+        midnight_black: '#10141E',
+        dark_blue: '#161D2F',
+        light_grey: '#5A698F',
+      },
+      fontFamily: {
+        sans: ['var(--font-outfit)'],
       },
     },
   },
